3000a3a7e7416776ab26669e609679076bc8609c
Denis Gladkikh <outcoldman@outlook.com>
2012-12-16 21:06:38 -0800

    show playlists

--- sources/Windows/GoogleMusic/BindingModels/PlaylistBindingModel.cs
+++ sources/Windows/GoogleMusic/BindingModels/PlaylistBindingModel.cs
@@ -3,10 +3,17 @@
 // --------------------------------------------------------------------------------------------------------------------
 namespace OutcoldSolutions.GoogleMusic.BindingModels
 {
+    using System;
+
     using OutcoldSolutions.GoogleMusic.WebServices.Models;
 
-    public class PlaylistBindingModel
+    using Windows.UI.Xaml.Media;
+    using Windows.UI.Xaml.Media.Imaging;
+
+    public class PlaylistBindingModel : BindingModelBase
     {
+        private static Random random = new Random();
+
         private readonly GoogleMusicPlaylist playlist;
 
         public PlaylistBindingModel(GoogleMusicPlaylist playlist)
@@ -19,6 +26,20 @@ namespace OutcoldSolutions.GoogleMusic.BindingModels
             get
             {
                 return this.playlist.Title;
+            }
+        }
+
+        public ImageSource PreviewImage
+        {
+            get
+            {
+                if (this.playlist.Playlist != null && this.playlist.Playlist.Count > 0)
+                {
+                    var index = random.Next(0, this.playlist.Playlist.Count - 1);
+                    return new BitmapImage(new Uri("https:" + this.playlist.Playlist[index].AlbumArtUrl));
+                }
+
+                return null;
             }
         }
     }
