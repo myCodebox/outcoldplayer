3bd1296a6e284f9c5a8466b5520af1e60049c871
Denis Gladkikh <outcoldman@outlook.com>
2013-01-13 10:12:24 -0800

    Start working on last.fm

---
+++ sources/Windows/GoogleMusic.Core/Web/Lastfm/ILastFmAccountWebService.cs
@@ -0,0 +1,14 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// Outcold Solutions (http://outcoldman.com)
+// --------------------------------------------------------------------------------------------------------------------
+namespace OutcoldSolutions.GoogleMusic.Web.LastFm
+{
+    using System.Threading.Tasks;
+
+    public interface ILastfmAccountWebService
+    {
+        Task<string> GetTokenAsync();
+
+        Task<string> GetSessionAsync(string token);
+    }
+}
---
+++ sources/Windows/GoogleMusic.Core/Web/Lastfm/LastfmSession.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// Outcold Solutions (http://outcoldman.com)
+// --------------------------------------------------------------------------------------------------------------------
+namespace OutcoldSolutions.GoogleMusic.Web.LastFm
+{
+    public class LastfmSession
+    {
+        public LastfmSession(string name, string key)
+        {
+            this.Name = name;
+            this.Key = key;
+        }
+
+        public string Name { get; private set; }
+
+        public string Key { get; private set; }
+    }
+}
