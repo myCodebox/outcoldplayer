b9833c1d114d7c41698f332c483f64d7992f7dfe
Denis Gladkikh <outcoldman@outlook.com>
2013-01-15 22:14:09 -0800

    Fix ordering issues - 0 means 1

--- sources/Windows/GoogleMusic.Core/Models/Genre.cs
+++ sources/Windows/GoogleMusic.Core/Models/Genre.cs
@@ -3,13 +3,20 @@
 // --------------------------------------------------------------------------------------------------------------------
 namespace OutcoldSolutions.GoogleMusic.Models
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
 
     public class Genre : Playlist
     {
         public Genre(string name, List<Song> songs)
-            : base(name, songs.OrderBy(s => s.GoogleMusicMetadata.ArtistNorm).OrderBy(s => s.GoogleMusicMetadata.AlbumNorm).ThenBy(s => s.GoogleMusicMetadata.Disc).ThenBy(s => s.GoogleMusicMetadata.Track).ToList())
+            : base(
+                name,
+                songs.OrderBy(s => s.GoogleMusicMetadata.ArtistNorm)
+                     .ThenBy(s => s.GoogleMusicMetadata.AlbumNorm)
+                     .ThenBy(s => Math.Max(s.GoogleMusicMetadata.Disc, 1))
+                     .ThenBy(s => s.GoogleMusicMetadata.Track)
+                     .ToList())
         {
         }
     }
--- sources/Windows/GoogleMusic.Core/Models/SystemPlaylist.cs
+++ sources/Windows/GoogleMusic.Core/Models/SystemPlaylist.cs
@@ -3,13 +3,14 @@
 // --------------------------------------------------------------------------------------------------------------------
 namespace OutcoldSolutions.GoogleMusic.Models
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
 
     public class SystemPlaylist : Playlist
     {
         public SystemPlaylist(string name, SystemPlaylistType type, IEnumerable<Song> songs)
-            : base(name, songs.OrderBy(s => s.GoogleMusicMetadata.ArtistNorm).OrderBy(s => s.GoogleMusicMetadata.AlbumNorm).ThenBy(s => s.GoogleMusicMetadata.Disc).ThenBy(s => s.GoogleMusicMetadata.Track).ToList())
+            : base(name, songs.OrderBy(s => s.GoogleMusicMetadata.ArtistNorm).ThenBy(s => s.GoogleMusicMetadata.AlbumNorm).ThenBy(s => Math.Max(s.GoogleMusicMetadata.Disc, 1)).ThenBy(s => s.GoogleMusicMetadata.Track).ToList())
         {
             this.Type = type;
             this.AlbumArtUrl = null;
