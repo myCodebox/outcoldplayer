c6a0f40e8b09c887ea17d73a26c004e343230f6d
Denis Gladkikh <outcoldman@outlook.com>
2013-01-29 21:06:55 -0800

    Change reauthentification on false result to reloadxsrt

--- sources/Windows/GoogleMusic.Core/Web/WebServiceBase.cs
+++ sources/Windows/GoogleMusic.Core/Web/WebServiceBase.cs
@@ -5,48 +5,26 @@ namespace OutcoldSolutions.GoogleMusic.Web
 {
     using System.Threading.Tasks;
 
-    using OutcoldSolutions.GoogleMusic.Services;
     using OutcoldSolutions.GoogleMusic.Web.Models;
 
     public abstract class WebServiceBase
     {
-        private readonly IGoogleAccountWebService googleAccountWebService;
-        private readonly IGoogleAccountService googleAccountService;
+        private const string RefreshXtUrl = "music/refreshxt";
+
         private readonly IGoogleMusicWebService googleMusicWebService;
-        private readonly IGoogleMusicSessionService sessionService;
 
         protected WebServiceBase(
-            IGoogleAccountWebService googleAccountWebService,
-            IGoogleAccountService googleAccountService,
-            IGoogleMusicWebService googleMusicWebService,
-            IGoogleMusicSessionService sessionService)
+            IGoogleMusicWebService googleMusicWebService)
         {
-            this.googleAccountWebService = googleAccountWebService;
-            this.googleAccountService = googleAccountService;
             this.googleMusicWebService = googleMusicWebService;
-            this.sessionService = sessionService;
         }
 
         protected async Task<bool> NeedRetry(CommonResponse commonResponse)
         {
             if (commonResponse.ReloadXsrf.HasValue && commonResponse.ReloadXsrf.Value)
             {
-                var userInfo = this.googleAccountService.GetUserInfo(retrievePassword: true);
-                if (userInfo != null)
-                {
-                    var googleLoginResponse = await this.googleAccountWebService.AuthenticateAsync(userInfo.Email, userInfo.Password);
-                    if (googleLoginResponse.Success)
-                    {
-                        var cookies = await this.googleAccountWebService.GetCookiesAsync(this.googleMusicWebService.GetServiceUrl());
-                        if (cookies != null)
-                        {
-                            this.googleMusicWebService.Initialize(cookies);
-                            return true;
-                        }
-                    }
-                }
-
-                this.sessionService.ClearSession();
+                await this.googleMusicWebService.PostAsync(RefreshXtUrl);
+                return true;
             }
 
             return false;
