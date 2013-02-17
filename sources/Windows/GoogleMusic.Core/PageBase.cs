3e058b4470b455f44031a1937cfba552667d5793
outcoldman <outcoldman@outlook.com>
2013-02-17 14:50:53 -0800

    Move base classes to cores

--- sources/Windows/GoogleMusic.Core/PageBase.cs
+++ sources/Windows/GoogleMusic.Core/PageBase.cs
@@ -1,7 +1,7 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // Outcold Solutions (http://outcoldman.com)
 // --------------------------------------------------------------------------------------------------------------------
-namespace OutcoldSolutions.GoogleMusic.Views
+namespace OutcoldSolutions.GoogleMusic
 {
     using OutcoldSolutions.Diagnostics;
 
