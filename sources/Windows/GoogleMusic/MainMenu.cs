79404454b005e3d492bcefcd1bdb93bcc97ebb40
outcoldman <outcoldman@outlook.com>
2013-04-13 10:41:42 -0700

    Add main menu item icons

--- sources/Windows/GoogleMusic/MainMenu.cs
+++ sources/Windows/GoogleMusic/MainMenu.cs
@@ -18,12 +18,12 @@ namespace OutcoldSolutions.GoogleMusic
 
         public static IEnumerable<MenuItemMetadata> GetItems()
         {
-            yield return MenuItemMetadata.FromViewType<IStartPageView>("Home");
-            yield return MenuItemMetadata.FromViewType<ICurrentPlaylistPageView>("Queue");
-            yield return MenuItemMetadata.FromViewType<IUserPlaylistsPageView>("Playlists", PlaylistType.UserPlaylist);
-            yield return MenuItemMetadata.FromViewType<IPlaylistsPageView>("Artists", PlaylistType.Artist);
-            yield return MenuItemMetadata.FromViewType<IPlaylistsPageView>("Albums", PlaylistType.Album);
-            yield return MenuItemMetadata.FromViewType<IPlaylistsPageView>("Genres", PlaylistType.Genre);
+            yield return MenuItemMetadata.FromViewType<IStartPageView>(new { Title = "Home", Icon = "ms-appx:///Resources/home.png" });
+            yield return MenuItemMetadata.FromViewType<ICurrentPlaylistPageView>(new { Title = "Queue", Icon = "ms-appx:///Resources/queue.png" });
+            yield return MenuItemMetadata.FromViewType<IUserPlaylistsPageView>(new { Title = "Playlists", Icon = "ms-appx:///Resources/playlists.png" }, PlaylistType.UserPlaylist);
+            yield return MenuItemMetadata.FromViewType<IPlaylistsPageView>(new { Title = "Artists", Icon = "ms-appx:///Resources/artists.png" }, PlaylistType.Artist);
+            yield return MenuItemMetadata.FromViewType<IPlaylistsPageView>(new { Title = "Albums", Icon = "ms-appx:///Resources/albums.png" }, PlaylistType.Album);
+            yield return MenuItemMetadata.FromViewType<IPlaylistsPageView>(new { Title = "Genres", Icon = "ms-appx:///Resources/genres.png" }, PlaylistType.Genre);
         }
     }
 }
