14a4431e2fd52a5abd6d443c37b33c7c32c19302
Denis Gladkikh <outcoldman@gmail.com>
2014-01-13 22:34:45 -0800

    Submitted updated version

--- sources/Windows/GoogleMusic.Core/Properties/AssemblyInfo.cs
+++ sources/Windows/GoogleMusic.Core/Properties/AssemblyInfo.cs
@@ -10,10 +10,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Outcold Solutions")]
 [assembly: AssemblyProduct("Outcold Solutions gMusicW")]
-[assembly: AssemblyCopyright("Outcold Solutions © 2013")]
+[assembly: AssemblyCopyright("Outcold Solutions © 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2.2.9.*")]
-[assembly: AssemblyFileVersion("2.2.9.0")]
+[assembly: AssemblyVersion("2.2.14.*")]
+[assembly: AssemblyFileVersion("2.2.14.0")]
 [assembly: ComVisible(false)]
