a9f896145c0f22b01965687cdbba083e452971c3
Denis Gladkikh <outcoldman@gmail.com>
2014-03-08 15:10:52 -0800

    Hide artists collection if he does not have them

---
+++ sources/Windows/GoogleMusic/Converters/NotEmptyToVisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// Outcold Solutions (http://outcoldman.com)
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OutcoldSolutions.GoogleMusic.Converters
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class NotEmptyToVisibilityConverter : VisibilityConverterBase 
+    {
+        public override object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var enumerable = value as IEnumerable<object>;
+            return this.ConvertToVisibility(enumerable != null && enumerable.Any());
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
