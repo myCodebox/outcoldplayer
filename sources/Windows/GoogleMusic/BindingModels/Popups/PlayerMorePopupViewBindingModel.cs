de6f3ea34473c2bc1d06dde0ee8624c28ff1af8b
Denis Gladkikh <outcoldman@gmail.com>
2014-03-11 21:58:41 -0700

    Move shuffle and repeat all to player view

--- sources/Windows/GoogleMusic/BindingModels/Popups/PlayerMorePopupViewBindingModel.cs
+++ sources/Windows/GoogleMusic/BindingModels/Popups/PlayerMorePopupViewBindingModel.cs
@@ -7,14 +7,12 @@ namespace OutcoldSolutions.GoogleMusic.BindingModels.Popups
     using System;
 
     using OutcoldSolutions.GoogleMusic.EventAggregator;
-    using OutcoldSolutions.GoogleMusic.Models;
     using OutcoldSolutions.GoogleMusic.Services;
     using OutcoldSolutions.GoogleMusic.Shell;
 
     public class PlayerMorePopupViewBindingModel : DisposableBindingModelBase
     {
         private readonly IMediaElementContainer mediaElementContainer;
-        private readonly IPlayQueueService playQueueService;
         private readonly IEventAggregator eventAggregator;
         private readonly IApplicationStateService stateService;
         private readonly IApplicationResources applicationResources;
@@ -22,54 +20,19 @@ namespace OutcoldSolutions.GoogleMusic.BindingModels.Popups
 
         public PlayerMorePopupViewBindingModel(
             IMediaElementContainer mediaElementContainer,
-            IPlayQueueService playQueueService,
             IEventAggregator eventAggregator,
             IApplicationStateService stateService,
             IApplicationResources applicationResources,
             IDispatcher dispatcher)
         {
             this.mediaElementContainer = mediaElementContainer;
-            this.playQueueService = playQueueService;
             this.eventAggregator = eventAggregator;
             this.stateService = stateService;
             this.applicationResources = applicationResources;
             this.dispatcher = dispatcher;
 
-            this.RegisterForDispose(this.eventAggregator.GetEvent<QueueChangeEvent>().Subscribe(
-                async (e) => await this.dispatcher.RunAsync(() =>
-                                {
-                                    this.RaisePropertyChanged(() => this.IsShuffleEnabled);
-                                    this.RaisePropertyChanged(() => this.IsRepeatAllEnabled);
-                                })));
-
             this.RegisterForDispose(this.eventAggregator.GetEvent<ApplicationStateChangeEvent>().Subscribe(
                 async (e) => await this.dispatcher.RunAsync(() => this.RaisePropertyChanged(() => this.IsOnlineMode))));
-        }
-
-        public bool IsShuffleEnabled
-        {
-            get
-            {
-                return this.playQueueService.IsShuffled;
-            }
-
-            set
-            {
-                this.playQueueService.IsShuffled = value;
-            }
-        }
-
-        public bool IsRepeatAllEnabled
-        {
-            get
-            {
-                return this.playQueueService.IsRepeatAll;
-            }
-
-            set
-            {
-                this.playQueueService.IsRepeatAll = value;
-            }
         }
 
         public double Volume
--- sources/Windows/GoogleMusic/Presenters/Popups/PlayerMorePopupViewPresenter.cs
+++ sources/Windows/GoogleMusic/Presenters/Popups/PlayerMorePopupViewPresenter.cs
@@ -6,56 +6,31 @@ namespace OutcoldSolutions.GoogleMusic.Presenters.Popups
     using System;
 
     using OutcoldSolutions.GoogleMusic.BindingModels.Popups;
-    using OutcoldSolutions.GoogleMusic.Services;
     using OutcoldSolutions.GoogleMusic.Shell;
     using OutcoldSolutions.GoogleMusic.Views.Popups;
 
     public class PlayerMorePopupViewPresenter : DisposableViewPresenterBase<IPlayerMorePopupView>, IDisposable
     {
-        private readonly IPlayQueueService queueService;
         private readonly IApplicationSettingViewsService applicationSettingViewsService;
 
         public PlayerMorePopupViewPresenter(
-            IPlayQueueService queueService,
             IApplicationSettingViewsService applicationSettingViewsService,
             PlayerMorePopupViewBindingModel bindingModel)
         {
-            this.queueService = queueService;
             this.applicationSettingViewsService = applicationSettingViewsService;
 
             this.BindingModel = bindingModel;
-
-            this.RepeatAllCommand = new DelegateCommand(() => { }, () => this.queueService.State != QueueState.Busy && !queueService.IsRadio);
-            this.ShuffleCommand = new DelegateCommand(() => { }, () => this.queueService.State != QueueState.Busy && !queueService.IsRadio);
+            
             this.ShowApplicationSettingsCommand = new DelegateCommand(async () =>
                 {
                     await this.Dispatcher.RunAsync(
                         () => this.applicationSettingViewsService.Show());
                 });
-
-            this.queueService.StateChanged += this.QueueServiceOnStateChanged;
         }
 
         public PlayerMorePopupViewBindingModel BindingModel { get; private set; }
 
-        public DelegateCommand ShuffleCommand { get; set; }
-
-        public DelegateCommand RepeatAllCommand { get; set; }
-
         public DelegateCommand ShowApplicationSettingsCommand { get; set; }
 
-        protected override void OnDisposing()
-        {
-            base.OnDisposing();
-
-            this.BindingModel.Dispose();
-            this.queueService.StateChanged -= this.QueueServiceOnStateChanged;
-        }
-
-        private void QueueServiceOnStateChanged(object sender, StateChangedEventArgs stateChangedEventArgs)
-        {
-            this.ShuffleCommand.RaiseCanExecuteChanged();
-            this.RepeatAllCommand.RaiseCanExecuteChanged();
-        }
     }
 }
