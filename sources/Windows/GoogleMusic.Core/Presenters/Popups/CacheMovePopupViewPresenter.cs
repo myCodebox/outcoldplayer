120ba0944814a9e7d90b99832b3356a444d61fe1
Denis Gladkikh <denis@gladkikh.email>
2014-11-15 15:05:36 -0800

    Handle errors in Move to Music Folder view #41

--- sources/Windows/GoogleMusic.Core/Presenters/Popups/CacheMovePopupViewPresenter.cs
+++ sources/Windows/GoogleMusic.Core/Presenters/Popups/CacheMovePopupViewPresenter.cs
@@ -17,6 +17,7 @@ namespace OutcoldSolutions.GoogleMusic.Presenters.Popups
         private readonly IPlayQueueService playQueueService;
         private readonly ISettingsService settingsService;
         private readonly IAnalyticsService analyticsService;
+        private readonly INotificationService notificationService;
         private readonly bool isMovingToMusicLibrary;
 
         private int totalFiles;
@@ -29,12 +30,14 @@ namespace OutcoldSolutions.GoogleMusic.Presenters.Popups
             IPlayQueueService playQueueService,
             ISettingsService settingsService,
             IAnalyticsService analyticsService,
+            INotificationService notificationService,
             bool isMovingToMusicLibrary)
         {
             this.songsCachingService = songsCachingService;
             this.playQueueService = playQueueService;
             this.settingsService = settingsService;
             this.analyticsService = analyticsService;
+            this.notificationService = notificationService;
             this.isMovingToMusicLibrary = isMovingToMusicLibrary;
         }
 
@@ -75,54 +78,71 @@ namespace OutcoldSolutions.GoogleMusic.Presenters.Popups
             await this.songsCachingService.CancelDownloadTaskAsync();
 
             StorageFolder appData = await this.songsCachingService.GetAppDataStorageFolderAsync();
-            StorageFolder musicLibrary = await this.songsCachingService.GetMusicLibraryStorageFolderAsync();
+            StorageFolder musicLibrary = null;
 
-            StorageFolder to = this.isMovingToMusicLibrary ? musicLibrary : appData;
-            StorageFolder from = this.isMovingToMusicLibrary ? appData : musicLibrary;
-
-            IList<StorageFile> allFiles = new List<StorageFile>();
-
-            foreach (var subfolder in await from.GetFoldersAsync())
+            try
             {
-                foreach (var file in await subfolder.GetFilesAsync())
-                {
-                    allFiles.Add(file);   
-                }
+                musicLibrary = await this.songsCachingService.GetMusicLibraryStorageFolderAsync();
+            }
+            catch (Exception e)
+            {
+                this.Logger.Debug(e, "Cannot get access to the music library");
             }
 
-            if (allFiles.Count > 0)
+            if (musicLibrary == null)
             {
-                await this.Dispatcher.RunAsync(() =>
-                {
-                    this.Message = "Moving cache files...";
-                    this.TotalFiles = allFiles.Count;
-                    this.FilesMoved = 0;
-                    this.IsCounterVisible = true;
-                });
+                await this.notificationService.ShowMessageAsync("Application could not get access to the Music Library folder. Please verify that you have permissions to this folder.");
+                this.View.Close();
+            }
+            else
+            {
+                StorageFolder to = this.isMovingToMusicLibrary ? musicLibrary : appData;
+                StorageFolder from = this.isMovingToMusicLibrary ? appData : musicLibrary;
 
-                foreach (var storageFile in allFiles)
-                {
-                    var toFile =
-                        await
-                            to.CreateFolderAsync(storageFile.Name.Substring(0, 1), CreationCollisionOption.OpenIfExists);
-                    await storageFile.MoveAsync(toFile);
+                IList<StorageFile> allFiles = new List<StorageFile>();
 
+                foreach (var subfolder in await from.GetFoldersAsync())
+                {
+                    foreach (var file in await subfolder.GetFilesAsync())
+                    {
+                        allFiles.Add(file);
+                    }
+                }
+
+                if (allFiles.Count > 0)
+                {
                     await this.Dispatcher.RunAsync(() =>
                     {
-                        this.FilesMoved++;
+                        this.Message = "Moving cache files...";
+                        this.TotalFiles = allFiles.Count;
+                        this.FilesMoved = 0;
+                        this.IsCounterVisible = true;
                     });
+
+                    foreach (var storageFile in allFiles)
+                    {
+                        var toFile =
+                            await
+                                to.CreateFolderAsync(storageFile.Name.Substring(0, 1), CreationCollisionOption.OpenIfExists);
+                        await storageFile.MoveAsync(toFile);
+
+                        await this.Dispatcher.RunAsync(() =>
+                        {
+                            this.FilesMoved++;
+                        });
+                    }
                 }
+
+                await from.DeleteAsync(StorageDeleteOption.PermanentDelete);
+
+                this.settingsService.SetIsMusicLibraryForCache(this.isMovingToMusicLibrary);
+
+                this.analyticsService.SendEvent("Settings", "ChangeIsMusicLibraryForCache", this.isMovingToMusicLibrary.ToString());
+
+                this.songsCachingService.StartDownloadTask();
+
+                this.View.Close();
             }
-
-            await from.DeleteAsync(StorageDeleteOption.PermanentDelete);
-
-            this.settingsService.SetIsMusicLibraryForCache(this.isMovingToMusicLibrary);
-
-            this.analyticsService.SendEvent("Settings", "ChangeIsMusicLibraryForCache", this.isMovingToMusicLibrary.ToString());
-
-            this.songsCachingService.StartDownloadTask();
-
-            this.View.Close();
         }
     }
 }
